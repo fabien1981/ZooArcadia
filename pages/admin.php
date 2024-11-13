733a7b22067676e8cc1e325898f48a4cd710cd07
fabien1981 <166442156+fabien1981@users.noreply.github.com>
2024-11-13 10:29:12 +0100

    modifications arborescence dossier

--- pages/admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once '../config/config.php';
-require_once '../templates/head.php';
-require_once '../templates/header.php';
-?>
-
-
-        <a href="../pages/ajout_animal.php">Ajouter un animal</a>
-        <a href="../pages/inscription.php">Créer un compte</a>
